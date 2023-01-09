d880a345472b78599d5872397a50279a695892f5
sabari-madhu <sabarim2131@gmail.com>
2023-01-09 13:17:13 +0530

    Delete prefect_integration directory

--- prefect_integration/models/postgres_test/orders.sql
+++
@@ -1,7 +0,0 @@
-with orders as (select user_id, order_date, status from {{ source('dbt', 'raw_orders') }}),
-customers as (select * from {{ source('dbt', 'raw_customers') }})
-
-select
-  *
-
-from  orders left join customers on orders.user_id = customers.id
